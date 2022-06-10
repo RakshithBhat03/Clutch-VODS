d6aff06ae0153a948131aee988ed199c684d934d
RakshithBhat03 <rakshith6666@gmail.com>
2022-06-10 15:05:40 +0530

    [chore]: updated exports

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { Navbar } from "./Navbar/Navbar";
 export { SideBar } from "./SideBar/SideBar";
 export { Card } from "./Card/Card";
 export { CategoryCard } from "./CategoryCard/CategoryCard";
+export { ShowToast } from "./ShowToast/ShowToast";
+export { FormError } from "./FormError/FormError";
