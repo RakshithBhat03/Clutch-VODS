6a8f234c100715f3cea8dacae1d5f6a480512ac0
RakshithBhat03 <rakshith6666@gmail.com>
2022-06-10 12:05:06 +0530

    [chore]: added exports

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { SplashScreen } from "./SplashScreen/SplashScreen";
+export { Navbar } from "./Navbar/Navbar";
+export { SideBar } from "./SideBar/SideBar";
+export { Card } from "./Card/Card";
+export { CategoryCard } from "./CategoryCard/CategoryCard";
