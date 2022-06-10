5d3f33f5a0b70f69f418caf6552e1e988ebf81e2
RakshithBhat03 <rakshith6666@gmail.com>
2022-06-10 15:08:52 +0530

    [feat]: added private and redirect routes

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import { SplashScreen } from "../components";
+import { RequireAuth } from "./RequireAuth";
 import {
   Explore,
   History,
@@ -15,12 +16,17 @@ export const AllRoutes = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/explore" element={<Explore />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/playlist" element={<Playlist />} />
-      <Route path="/history" element={<History />} />
-      <Route path="/watch-later" element={<WatchLater />} />
-      <Route path="/liked-videos" element={<LikedVideos />} />
+
+      <Route element={<RequireAuth isLoggedIn />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Route>
+      <Route element={<RequireAuth />}>
+        <Route path="/playlist" element={<Playlist />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/watch-later" element={<WatchLater />} />
+        <Route path="/liked-videos" element={<LikedVideos />} />
+      </Route>
       <Route
         path="*"
         element={<SplashScreen text="Looks like you're lost" notFound={true} />}
