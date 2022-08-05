d02f8814faffc82c4e7d0d194a68cb1966a7870a
RakshithBhat03 <rakshith6666@gmail.com>
2022-08-06 00:51:35 +0530

    [feat]: added logo

--- src/components/SinglePlaylist/SinglePlaylist.jsx
+++ src/components/SinglePlaylist/SinglePlaylist.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { useDocumentTitle } from "../../hooks/useDocumentTitle";
 import { Card } from "../Card/Card";
 import { SplashScreen } from "../SplashScreen/SplashScreen";
 
@@ -10,10 +11,16 @@ const SinglePlaylist = () => {
   const getPlaylist = (playlists, id) =>
     playlists.find(({ _id }) => _id === id);
   const currentPlaylist = getPlaylist(playlists, id);
-
+  useDocumentTitle(`${currentPlaylist.title} | Clutch VODS`);
   return currentPlaylist?.videos?.length > 0 ? (
     <div className="flex-1 ml-20 md:ml-0 p-1">
-      <div className="box-shadow--theme grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
+      <div className="my-2">
+        <h2 className="sm:text-3xl text-2xl my-2 font-semibold">
+          Playlist : {currentPlaylist.title}
+        </h2>
+        <p>Description : {currentPlaylist.description}</p>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
         {currentPlaylist.videos.map((video, index) => (
           <Card video={video} key={index} deleteBtn playlistVideo />
         ))}
